593cfe15b5748357076c4310d33ad646a0271a0d
Piotr_Piorkowski <p.piorkowski@gr8soft.pl>
2022-12-19 16:16:06 +0100

    build fix

--- components/ProductReview/ProductReviewList.tsx
+++ components/ProductReview/ProductReviewList.tsx
@@ -19,6 +19,7 @@ export const ProductReviewList = ({ productSlug }: ProductReviewListProps) => {
     return null;
   }
 
+  //items
   return (
     <ul>
       {data.product.reviews.map((review, i) => (
